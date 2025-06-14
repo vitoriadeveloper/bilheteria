c597f8aa1333ed04060afe1cc79cc27b8eb86d1c
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-06-14 11:50:29 -0300

    [FEAT] - implementing set error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, res) => {
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: "Erro de validação de schema", issues: error.format() });
+  }
+  return res.status(500).send({ message: "Erro interno do servidor" });
+});
