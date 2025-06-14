6a20bd48adba43cab5972d840e6fcf8ba4f25007
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-06-14 17:56:53 -0300

    [FEAT] - implementing new method at users repository

--- src/repository/prisma/prisma-users-repository.ts
+++ src/repository/prisma/prisma-users-repository.ts
@@ -3,6 +3,13 @@ import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findFirst({
+      where: { id },
+    });
+
+    return user;
+  }
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
