d100328eb28ce47aa9d1949c67d30642ba93380e
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-06-14 13:59:54 -0300

    [FEAT] - implementing login path

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
+import { authenticate } from "./controllers/authenticate";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/usuarios", register);
+    app.post("/login", authenticate)
 }
