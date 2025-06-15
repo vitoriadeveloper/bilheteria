d2a7fcc9766c6999e03c3a5b28066fc72ef12330
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-06-15 11:30:26 -0300

    [FIX] - name of factory refactored

--- src/services/factories/make-get-all-movies-service.ts
+++ src/services/factories/make-get-all-movies-service.ts
@@ -1,9 +1,9 @@
 import { PrismaMoviesRepository } from "@/repository/prisma/prisma-movies-repository";
 import { GetAllMoviesService } from "../get-all-movies";
 
-export function makeGetAllMoviesService(){
-    const prismaMoviesRepository = new PrismaMoviesRepository();
-    const getAllMoviesRepository = new GetAllMoviesService(prismaMoviesRepository);
+export function makeGetAllMoviesService() {
+  const prismaMoviesRepository = new PrismaMoviesRepository();
+  const getAllMoviesService = new GetAllMoviesService(prismaMoviesRepository);
 
-    return getAllMoviesRepository
+  return getAllMoviesService;
 }
