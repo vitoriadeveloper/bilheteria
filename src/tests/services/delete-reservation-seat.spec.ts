9c35b76eca6506699826d2d4163a92170249eeac
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-06-15 11:05:26 -0300

    [TEST] - implementing  new unit test scenario

--- src/tests/services/delete-reservation-seat.spec.ts
+++ src/tests/services/delete-reservation-seat.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryReservationRepository } from "@/repository/in-memory/in-memory-reservation-repository";
 import { DeleteReservationSeatService } from "@/services/delete-reservation-seat";
+import { ReservationAlreadyConfirmed } from "@/services/errors/reservation-already-confirmed";
 import { ReservationNotFoundError } from "@/services/errors/reservation-not-found";
 import { randomUUID } from "crypto";
 import { describe, it, expect, beforeEach } from "vitest";
@@ -31,5 +32,18 @@ describe("Delete Reservation seat service", () => {
     await expect(() =>
       sut.execute({ reservationId: "12425" })
     ).rejects.toBeInstanceOf(ReservationNotFoundError);
+  });
+  it("Não deve deletar uma reserva já confirmada", async () => {
+    const reservation = await inMemoryReservationRepository.create({
+      id: randomUUID(),
+      userId: "user-2",
+      seatId: "seat-2",
+      expiresAt: new Date(Date.now() + 15 * 60 * 1000),
+      confirmed: true,
+    });
+
+    await expect(() =>
+      sut.execute({ reservationId: reservation.id })
+    ).rejects.toBeInstanceOf(ReservationAlreadyConfirmed);
   });
 });
